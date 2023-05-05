38f49547c35e8cd797ce6d19d1e2966c3ed228f5
Joel Sprunger <joelsprunger@Joels-MacBook-Pro.local>
2023-05-05 13:29:46 -0700

    dissables qa functionality for testing depoloyment

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from website import app
 from os import environ
 
-app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
+# app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
+app.config["SECRET_KEY"] = "mysecretkey"
 
 if __name__ == "__main__":
     app.run()
