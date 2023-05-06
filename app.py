b153fce71de6456c4e68c6fdd194c01acee34cf1
Joel Sprunger <joelsprunger@Joels-MacBook-Pro.local>
2023-05-05 22:51:39 -0700

    makes secret key an environment variable

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from website import app
 from os import environ
 
-# app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
-app.config["SECRET_KEY"] = "mysecretkey"
+app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
 
 if __name__ == "__main__":
     app.run(debug=True)
