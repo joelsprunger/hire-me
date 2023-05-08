8a29feb320e845f1df64df7a9dd971a576d6e4e9
Joel Sprunger <joelsprunger@Joels-MacBook-Pro.local>
2023-05-08 12:53:57 -0700

    Adds session to the app so that each session gets it's own welcome notification, and hire-me button reveal.

--- website/core/views.py
+++ website/core/views.py
@@ -1,11 +1,12 @@
-from flask import render_template, Blueprint, flash, send_file, send_from_directory
+from flask import render_template, Blueprint, flash, send_from_directory, session
 from .forms import QuestionForm, EmailForm
 from ..ai_service.ai_answers import string_answer
 from .. import mail, Message
+from random import randint as randi
 
 core = Blueprint("core", __name__)
 
-questions: dict = {}  # ordered dict eliminates repeated calls with the same question
+questions: dict[dict] = {}  # ordered dict eliminates repeated calls with the same question
 qa_list: list = []    # ordered list allows for printing questions in reverse order and repeated questions
 start: bool = True
 intro_message: bool = True
@@ -13,6 +14,9 @@ intro_message: bool = True
 
 @core.route("/", methods=["GET", "POST"])
 def index():
+    if "n_questions" not in session.keys():
+        session["n_questions"] = 0
+    n_questions = session["n_questions"]
     form = QuestionForm()
     email_form = EmailForm()
     if questions:
@@ -22,6 +26,8 @@ def index():
 
     # process Q/A form
     if form.validate_on_submit():
+        n_questions += 1
+        session["n_questions"] = n_questions
         question = form.question.data
         form.question.data = None  # clear form so that placeholder question is shown
         # check for repeated question
@@ -42,6 +48,7 @@ def index():
                            form=form,
                            qa=qa_list,
                            question=question,
+                           n_questions=n_questions,
                            email_form=email_form)
 
 
