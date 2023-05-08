e847910ce7d225b1cf7df4d46a382f954e9bb34a
Joel Sprunger <joelsprunger@Joels-MacBook-Pro.local>
2023-05-08 08:02:55 -0700

    clears form after each question so that it doesn't need to be deleted

--- website/core/views.py
+++ website/core/views.py
@@ -3,18 +3,24 @@ from .forms import QuestionForm
 from ..ai_service.ai_answers import string_answer
 
 core = Blueprint("core", __name__)
+
 qa = []
 
 
 @core.route("/", methods=["GET", "POST"])
 def index():
     form = QuestionForm()
+    if qa:
+        question = qa[-1][0]  # Use most recent question
+    else:
+        question = "Question"  # Use initial prompt
     if form.validate_on_submit():
         question = form.question.data
+        form.question.data = None  # clear form so that placeholder question is shown
         answer = string_answer(question)
         qa.append((question, answer))
         flash(answer)
-    return render_template("index.html", form=form, qa=qa)
+    return render_template("index.html", form=form, qa=qa, question=question)
 
 
 @core.route("/about")
